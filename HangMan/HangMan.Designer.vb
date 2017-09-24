f8aaddce7054e710d3ad0ca737072608293189d3
jabranCodeMaster <32246700+jabranCodeMaster@users.noreply.github.com>
2017-09-24 19:32:09 -0400

    Add files via upload

---
+++ HangMan/HangMan.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HangMan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblChar1 = New System.Windows.Forms.Label()
+        Me.lblChar2 = New System.Windows.Forms.Label()
+        Me.lblChar3 = New System.Windows.Forms.Label()
+        Me.lblChar4 = New System.Windows.Forms.Label()
+        Me.lblChar5 = New System.Windows.Forms.Label()
+        Me.lblChar6 = New System.Windows.Forms.Label()
+        Me.lblChar7 = New System.Windows.Forms.Label()
+        Me.lblChar8 = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.txtEnter = New System.Windows.Forms.TextBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox0 = New System.Windows.Forms.PictureBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox0, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblChar1
+        '
+        Me.lblChar1.AutoSize = True
+        Me.lblChar1.Location = New System.Drawing.Point(61, 373)
+        Me.lblChar1.Name = "lblChar1"
+        Me.lblChar1.Size = New System.Drawing.Size(25, 13)
+        Me.lblChar1.TabIndex = 0
+        Me.lblChar1.Text = "___"
+        '
+        'lblChar2
+        '
+        Me.lblChar2.AutoSize = True
+        Me.lblChar2.Location = New System.Drawing.Point(92, 373)
+        Me.lblChar2.Name = "lblChar2"
+        Me.lblChar2.Size = New System.Drawing.Size(25, 13)
+        Me.lblChar2.TabIndex = 1
+        Me.lblChar2.Text = "___"
+        '
+        'lblChar3
+        '
+        Me.lblChar3.AutoSize = True
+        Me.lblChar3.Location = New System.Drawing.Point(123, 373)
+        Me.lblChar3.Name = "lblChar3"
+        Me.lblChar3.Size = New System.Drawing.Size(25, 13)
+        Me.lblChar3.TabIndex = 2
+        Me.lblChar3.Text = "___"
+        '
+        'lblChar4
+        '
+        Me.lblChar4.AutoSize = True
+        Me.lblChar4.Location = New System.Drawing.Point(154, 373)
+        Me.lblChar4.Name = "lblChar4"
+        Me.lblChar4.Size = New System.Drawing.Size(25, 13)
+        Me.lblChar4.TabIndex = 3
+        Me.lblChar4.Text = "___"
+        '
+        'lblChar5
+        '
+        Me.lblChar5.AutoSize = True
+        Me.lblChar5.Location = New System.Drawing.Point(185, 373)
+        Me.lblChar5.Name = "lblChar5"
+        Me.lblChar5.Size = New System.Drawing.Size(25, 13)
+        Me.lblChar5.TabIndex = 4
+        Me.lblChar5.Text = "___"
+        '
+        'lblChar6
+        '
+        Me.lblChar6.AutoSize = True
+        Me.lblChar6.Location = New System.Drawing.Point(211, 373)
+        Me.lblChar6.Name = "lblChar6"
+        Me.lblChar6.Size = New System.Drawing.Size(25, 13)
+        Me.lblChar6.TabIndex = 5
+        Me.lblChar6.Text = "___"
+        '
+        'lblChar7
+        '
+        Me.lblChar7.AutoSize = True
+        Me.lblChar7.Location = New System.Drawing.Point(242, 373)
+        Me.lblChar7.Name = "lblChar7"
+        Me.lblChar7.Size = New System.Drawing.Size(25, 13)
+        Me.lblChar7.TabIndex = 6
+        Me.lblChar7.Text = "___"
+        '
+        'lblChar8
+        '
+        Me.lblChar8.AutoSize = True
+        Me.lblChar8.Location = New System.Drawing.Point(273, 373)
+        Me.lblChar8.Name = "lblChar8"
+        Me.lblChar8.Size = New System.Drawing.Size(25, 13)
+        Me.lblChar8.TabIndex = 7
+        Me.lblChar8.Text = "___"
+        '
+        'btnStart
+        '
+        Me.btnStart.BackColor = System.Drawing.Color.ForestGreen
+        Me.btnStart.Location = New System.Drawing.Point(11, 13)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(199, 91)
+        Me.btnStart.TabIndex = 8
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = False
+        '
+        'txtEnter
+        '
+        Me.txtEnter.Location = New System.Drawing.Point(311, 296)
+        Me.txtEnter.Name = "txtEnter"
+        Me.txtEnter.Size = New System.Drawing.Size(233, 20)
+        Me.txtEnter.TabIndex = 9
+        Me.txtEnter.Text = "Enter a letter"
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(311, 322)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(233, 61)
+        Me.btnEnter.TabIndex = 10
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.HangMan.My.Resources.Resources.hangman1
+        Me.PictureBox1.Location = New System.Drawing.Point(699, 51)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(126, 109)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox0
+        '
+        Me.PictureBox0.Image = Global.HangMan.My.Resources.Resources.hangman0
+        Me.PictureBox0.Location = New System.Drawing.Point(550, 13)
+        Me.PictureBox0.Name = "PictureBox0"
+        Me.PictureBox0.Size = New System.Drawing.Size(351, 371)
+        Me.PictureBox0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox0.TabIndex = 11
+        Me.PictureBox0.TabStop = False
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 167)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(39, 13)
+        Me.Label9.TabIndex = 13
+        Me.Label9.Text = "Label9"
+        '
+        'HangMan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gold
+        Me.ClientSize = New System.Drawing.Size(926, 395)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox0)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.txtEnter)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.lblChar8)
+        Me.Controls.Add(Me.lblChar7)
+        Me.Controls.Add(Me.lblChar6)
+        Me.Controls.Add(Me.lblChar5)
+        Me.Controls.Add(Me.lblChar4)
+        Me.Controls.Add(Me.lblChar3)
+        Me.Controls.Add(Me.lblChar2)
+        Me.Controls.Add(Me.lblChar1)
+        Me.Name = "HangMan"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox0, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblChar1 As System.Windows.Forms.Label
+    Friend WithEvents lblChar2 As System.Windows.Forms.Label
+    Friend WithEvents lblChar3 As System.Windows.Forms.Label
+    Friend WithEvents lblChar4 As System.Windows.Forms.Label
+    Friend WithEvents lblChar5 As System.Windows.Forms.Label
+    Friend WithEvents lblChar6 As System.Windows.Forms.Label
+    Friend WithEvents lblChar7 As System.Windows.Forms.Label
+    Friend WithEvents lblChar8 As System.Windows.Forms.Label
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+    Friend WithEvents txtEnter As System.Windows.Forms.TextBox
+    Friend WithEvents btnEnter As System.Windows.Forms.Button
+    Friend WithEvents PictureBox0 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+
+End Class
---
+++ HangMan/HangMan.vb
@@ -0,0 +1,207 @@
+﻿Imports System.IO
+Public Class HangMan
+    'Jabran Khan
+    '6/4/13
+    Public strWord As String
+    Public blnPic1, blnPic2, blnPic3, blnPic4, blnPic5, blnPic6, blnPic7, blnPic8 As Boolean
+    Public blnLet1, blnLet2, blnLet3, blnLet4, blnLet5, blnLet6, blnLet7, blnLet8 As Boolean
+
+    Private Sub btnStart_Click(sender As System.Object, e As System.EventArgs) Handles btnStart.Click
+        'Makes sure the user doesn't try to get a new word :)
+        'btnStart.Enabled = False
+
+        'Makes the parts of the hangman disappear to start the next round
+        PictureBox1.Visible = False
+        blnPic1 = False
+        blnPic2 = False
+        'Randomly choosing a word when the user clicks the start button
+        Randomize()
+        Dim intWord As Integer
+        intWord = CInt(Int((5 * Rnd()) + 1))
+        Select Case intWord
+            Case 1
+                strWord = "Anime"
+            Case 2
+                strWord = "Chess"
+            Case 3
+                strWord = "Manga"
+            Case 4
+                strWord = "Tennis"
+            Case 5
+                strWord = "Football"
+            Case 6
+                strWord = ""
+            Case 7
+                strWord = ""
+            Case 8
+                strWord = ""
+            Case 9
+                strWord = ""
+            Case 10
+                strWord = ""
+            Case 11
+                strWord = ""
+            Case 12
+                strWord = ""
+            Case 13
+                strWord = ""
+            Case 14
+                strWord = ""
+            Case 15
+                strWord = ""
+            Case 16
+                strWord = ""
+            Case 17
+                strWord = ""
+            Case 18
+                strWord = ""
+            Case 19
+                strWord = ""
+            Case 20
+                strWord = ""
+            Case 21
+                strWord = ""
+            Case 22
+                strWord = ""
+            Case 23
+                strWord = ""
+            Case 24
+                strWord = ""
+            Case 25
+                strWord = ""
+        End Select
+        Label9.Text = strWord
+
+        'The amount of blank lines that show up
+        'to tell the user how many letters are there
+        Dim intChar As Integer
+        intChar = strWord.Length
+        If intChar = 3 Then
+            lblChar1.Visible = True
+            lblChar2.Visible = True
+            lblChar3.Visible = True
+            lblChar4.Visible = False
+            lblChar5.Visible = False
+            lblChar6.Visible = False
+            lblChar7.Visible = False
+            lblChar8.Visible = False
+        End If
+        If intChar = 4 Then
+            lblChar1.Visible = True
+            lblChar2.Visible = True
+            lblChar3.Visible = True
+            lblChar4.Visible = True
+            lblChar5.Visible = False
+            lblChar6.Visible = False
+            lblChar7.Visible = False
+            lblChar8.Visible = False
+        End If
+        If intChar = 5 Then
+            lblChar1.Visible = True
+            lblChar2.Visible = True
+            lblChar3.Visible = True
+            lblChar4.Visible = True
+            lblChar5.Visible = True
+            lblChar6.Visible = False
+            lblChar7.Visible = False
+            lblChar8.Visible = False
+        End If
+        If intChar = 6 Then
+            lblChar1.Visible = True
+            lblChar2.Visible = True
+            lblChar3.Visible = True
+            lblChar4.Visible = True
+            lblChar5.Visible = True
+            lblChar6.Visible = True
+            lblChar7.Visible = False
+            lblChar8.Visible = False
+        End If
+        If intChar = 7 Then
+            lblChar1.Visible = True
+            lblChar2.Visible = True
+            lblChar3.Visible = True
+            lblChar4.Visible = True
+            lblChar5.Visible = True
+            lblChar6.Visible = True
+            lblChar7.Visible = True
+            lblChar8.Visible = False
+        End If
+        If intChar = 8 Then
+            lblChar1.Visible = True
+            lblChar2.Visible = True
+            lblChar3.Visible = True
+            lblChar4.Visible = True
+            lblChar5.Visible = True
+            lblChar6.Visible = True
+            lblChar7.Visible = True
+            lblChar8.Visible = True
+        End If
+    End Sub
+
+    Private Sub btnEnter_Click(sender As System.Object, e As System.EventArgs) Handles btnEnter.Click
+        'Check the letters and whether they are in the word or not
+        'If they are, then do the following:
+        Dim strLetter As String
+        Dim i, intTally, intlength, oneLet, twoLet As Integer
+        intTally = 0 'How many of that letter are there in the word
+        strLetter = txtEnter.Text
+        intlength = strWord.Length + 1
+        For i = 0 To strWord.Length - 1
+            If strWord.Substring(i, 1) = strLetter Then
+                intTally = intTally + 1
+            End If
+        Next
+
+        'If the word has one of that letter
+        If intTally = 1 Then
+            oneLet = strWord.IndexOf(strLetter)
+        End If
+        'If the word has two of that letter
+        If intTally = 2 Then
+            oneLet = strWord.IndexOf(strLetter)
+            twoLet = strWord.IndexOf(strLetter, oneLet + 1)
+        End If
+
+        'Display the letter in the blanks
+        'In the first blank
+        If oneLet = 1 Or twoLet = 1 Then
+            lblChar1.Text = strLetter
+            blnLet1 = True
+        End If
+        'In the second blank
+        If oneLet = 2 Or twoLet = 2 Then
+            lblChar2.Text = strLetter
+            blnLet2 = True
+        End If
+        'In the third blank
+        If oneLet = 3 Or twoLet = 3 Then
+            lblChar4.Text = strLetter
+            blnLet3 = True
+        End If
+        'In the fourth blank
+        If oneLet = 4 Or twoLet = 4 Then
+            lblChar4.Text = strLetter
+            blnLet4 = True
+        End If
+        'In the fifth blank
+        If oneLet = 5 Or twoLet = 5 Then
+            lblChar5.Text = strLetter
+            blnLet5 = True
+        End If
+        'In the sixth blank
+        If oneLet = 6 Or twoLet = 6 Then
+            lblChar6.Text = strLetter
+            blnLet6 = True
+        End If
+        'In the seventh blank
+        If oneLet = 7 Or twoLet = 7 Then
+            lblChar7.Text = strLetter
+            blnLet7 = True
+        End If
+        'In the eighth blank
+        If oneLet = 8 Or twoLet = 8 Then
+            lblChar8.Text = strLetter
+            blnLet8 = True
+        End If
+    End Sub
+End Class
